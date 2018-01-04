df751a18825b7aaac7e1c1ab643172df7b204cb2
Flavius12 <info@evolsoft.tk>
2018-01-04 17:40:15 +0100

    Updated to PocketMine-MP 1.7dev

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-
 ![start2](https://cloud.githubusercontent.com/assets/10303538/6315586/9463fa5c-ba06-11e4-8f30-ce7d8219c27d.png)
+
 # PerWorldAlerts
 
 Per World Alerts plugin for PocketMine-MP
@@ -10,21 +10,25 @@ PocketMine-MP plugins
 
 ## Requirements
 
-PocketMine-MP Alpha_1.4 API 1.9.0
+PocketMine-MP 1.7dev API 3.0.0-ALPHA7, 3.0.0-ALPHA8, 3.0.0-ALPHA9
 
 ## Overview
 
-**PerWorldAlerts** allows you to have single alerts (join/leave messages, etc...) in each world.
+**PerWorldAlerts** let you have separate alerts (join/leave messages, etc...) for each world.
 
-**EvolSoft Website:** http://www.evolsoft.tk
+**EvolSoft Website:** https://www.evolsoft.tk
 
 ***This Plugin uses the New API. You can't install it on old versions of PocketMine.***
 
-With PerWorldAlerts you can single alerts (join/leave messages, etc...) in each world and you can disable alerts in worlds. (read documentation)
+With PerWorldAlerts you can have separate alerts (join/leave messages, etc...) for each world and you can also disable alerts in other worlds. (read documentation)
 
 **To-Do:**
 
 *- Bug fix (if bugs will be found)*
+
+## Donate
+
+Support the development of this plugin with a small donation by clicking [:dollar: here](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=flavius.c.1999@gmail.com&lc=US&item_name=www.evolsoft.tk&no_note=0&cn=&curency_code=EUR&bn=PP-DonationsBF:btn_donateCC_LG.gif:NonHosted). Thank you :smile:
 
 ## Documentation 
 
--- src/PerWorldAlerts/EventListener.php
+++ src/PerWorldAlerts/EventListener.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * PerWorldAlerts (v1.1) by EvolSoft
+ * PerWorldAlerts (v1.2) by EvolSoft
  * Developer: EvolSoft (Flavius12)
- * Website: http://www.evolsoft.tk
- * Date: 28/12/2014 12:52 PM (UTC)
- * Copyright & License: (C) 2014 EvolSoft
+ * Website: https://www.evolsoft.tk
+ * Date: 04/01/2018 04:38 PM (UTC)
+ * Copyright & License: (C) 2014-2018 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/PerWorldAlerts/blob/master/LICENSE)
  */
 
@@ -17,49 +17,54 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat;
 
-class EventListener extends PluginBase implements Listener{
+class EventListener extends PluginBase implements Listener {
 	
-	public function __construct(Main $plugin){
+	public function __construct(PerWorldAlerts $plugin){
 		$this->plugin = $plugin;
 	}
 	
+	/**
+	 * @param PlayerJoinEvent $event
+	 */
 	public function onPlayerJoin(PlayerJoinEvent $event){
 		$player = $event->getPlayer();
 		$message = $event->getJoinMessage();
 		$event->setJoinMessage("");
-		if($message != "" || $message != " "){
-			if($this->plugin->isAlertDisabled($player->getLevel()->getName()) == false){
-				$this->plugin->SendLevelAlert($player->getLevel()->getName(), $message);
+		if($message != null && !ctype_space($message)){
+			if(!$this->plugin->isAlertDisabled($player->getLevel()->getName())){
+				$this->plugin->sendLevelAlert($player->getLevel(), $message);
 			}
 		}
 	}
 	
+	/**
+	 * @param PlayerQuitEvent $event
+	 */
 	public function onPlayerQuit(PlayerQuitEvent $event){
 		$player = $event->getPlayer();
 		$message = $event->getQuitMessage();
 		$event->setQuitMessage("");
-		if($message != "" || $message != " "){
-			if($this->plugin->isAlertDisabled($player->getLevel()->getName()) == false){
-				$this->plugin->SendLevelAlert($player->getLevel()->getName(), $message);
+		if($message != null && !ctype_space($message)){
+			if(!$this->plugin->isAlertDisabled($player->getLevel()->getName())){
+				$this->plugin->sendLevelAlert($player->getLevel(), $message);
 			}
 		}
 	}
 	
+	/**
+	 * @param PlayerDeathEvent $event
+	 */
 	public function onPlayerDeath(PlayerDeathEvent $event){
 		$player = $event->getEntity();
 		if($player instanceof Player){
 			$message = $event->getDeathMessage();
 			$event->setDeathMessage("");
-			if($message != "" || $message != " "){
-				if($this->plugin->isAlertDisabled($player->getLevel()->getName()) == false){
-					$this->plugin->SendLevelAlert($player->getLevel()->getName(), $message);
+			if($message != null && !ctype_space($message)){
+				if(!$this->plugin->isAlertDisabled($player->getLevel()->getName())){
+					$this->plugin->sendLevelAlert($player->getLevel(), $message);
 				}
 			}
 		}
 	}
-	
 }
-?>
